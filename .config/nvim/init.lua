b6ee9d4e0c27137f7687fb939336a074b3770bfc
Dilitha <diliw101@gmail.com>
2025-02-14 13:52:28 +1300

    Remove typo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
--- Set <space> as the leader keymason
+-- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
